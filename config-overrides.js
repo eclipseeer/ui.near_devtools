c29aa23d97f580724393336c44cd4e908a3f67c8
eclipseeer <eclipseeer@gmail.com>
2021-12-22 19:08:14 +0200

    Add @emotion

---
+++ config-overrides.js
@@ -0,0 +1,6 @@
+const { override, addBabelPlugin, addBabelPreset } = require('customize-cra');
+
+module.exports = override(
+  addBabelPreset(['@babel/preset-react', { runtime: 'automatic', importSource: '@emotion/react' }]),
+  addBabelPlugin('@emotion/babel-plugin'),
+);
--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -1,1 +1,11 @@
-export const App = () => <div>App</div>;
+import { css } from '@emotion/react';
+
+const container = css`
+  background-color: red;
+  color: aliceblue;
+  @media (min-width: 1000px) {
+    background-color: black;
+  }
+`;
+
+export const App = () => <div css={container}>App</div>;
